08180712f4b60bf41b62f751f546845a39e44e88
Ruben <ruben.bergshoeff@littlechicken.nl>
2019-06-06 17:09:05 +0200

    updated voicebehaviour to safely work with unmanaged land

--- UnityProject/FMODCallbackTutorial/Assets/CORE/Scripts/Audio/VoiceBehaviour.cs
+++ UnityProject/FMODCallbackTutorial/Assets/CORE/Scripts/Audio/VoiceBehaviour.cs
@@ -4,47 +4,84 @@ using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
 using System;
+using System.Runtime.InteropServices;
 
 public class VoiceBehaviour : MonoBehaviour
 {
+    private class VoiceStateInfo {
+        public VoiceState State;
+    }
+    private enum VoiceState {
+        Talking,
+        Silent
+    }
+
     [EventRef] public string FMODPathHit;
 
     private EVENT_CALLBACK voiceCallback;
     private EventInstance eventInstanceVoice;
-    private bool isTalking;
+    private VoiceStateInfo voiceInfo;
+    private GCHandle voiceInfoHandle;
 
     private void Start() {
         voiceCallback = new EVENT_CALLBACK(VoiceEventCallback);
+
+        // create and pin voiceinfo in memory
+        voiceInfo = new VoiceStateInfo();
+        voiceInfo.State = VoiceState.Silent;
+        voiceInfoHandle = GCHandle.Alloc(voiceInfo, GCHandleType.Pinned);
     }
 
     private void OnMouseDown() {
         PlayVoiceLine(FMODPathHit);
     }
 
-    private void PlayVoiceLine(string fmodPath) {
-        // if character is talking, return and do not continue with method
-        if (isTalking) {
+    private void PlayVoiceLine(string voiceLinePath) {
+        if(voiceInfo.State == VoiceState.Talking) {
             return;
         }
 
-        isTalking = true;
-        eventInstanceVoice = RuntimeManager.CreateInstance(fmodPath);
+        voiceInfo.State = VoiceState.Talking;
+        eventInstanceVoice = RuntimeManager.CreateInstance(voiceLinePath);
+        eventInstanceVoice.setUserData(GCHandle.ToIntPtr(voiceInfoHandle));
         eventInstanceVoice.setCallback(voiceCallback);
         eventInstanceVoice.start();
     }
 
     [AOT.MonoPInvokeCallback(typeof(EVENT_CALLBACK))]
     private FMOD.RESULT VoiceEventCallback(EVENT_CALLBACK_TYPE callbackType, EventInstance eventInstance, IntPtr parameters) {
-        switch (callbackType) {
-            case EVENT_CALLBACK_TYPE.START_FAILED:
-            case EVENT_CALLBACK_TYPE.STOPPED:
-                isTalking = false;
-                break;
+        IntPtr voiceStatePtr;
+        FMOD.RESULT result = eventInstance.getUserData(out voiceStatePtr);
+
+        if(result != FMOD.RESULT.OK) {
+            Debug.LogError("FMOD error: " + result);
         }
+        else {
+            if(voiceStatePtr != IntPtr.Zero) {
+                GCHandle voiceInfoHandle = GCHandle.FromIntPtr(voiceStatePtr);
+                VoiceStateInfo voiceInfo = (VoiceStateInfo)voiceInfoHandle.Target;
+
+                switch (callbackType) {
+                    case EVENT_CALLBACK_TYPE.START_FAILED:
+                    case EVENT_CALLBACK_TYPE.STOPPED:
+                        eventInstance.setUserData(IntPtr.Zero);
+                        eventInstance.setCallback(null);
+                        eventInstance.release();
+                        voiceInfo.State = VoiceState.Silent;
+                        break;
+                }
+            }
+        }
+
         return FMOD.RESULT.OK;
     }
 
     private void OnDestroy() {
-        eventInstanceVoice.setCallback(null);
+        if(voiceInfo.State == VoiceState.Talking) {
+            eventInstanceVoice.setUserData(IntPtr.Zero);
+            eventInstanceVoice.setCallback(null);
+            eventInstanceVoice.release();
+        }
+        voiceInfoHandle.Free();
     }
 }
